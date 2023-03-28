604908f6c4fe0c2f8e74fc84036a31c738c884ed
saikrishna200819 <128981212+saikrishna200819@users.noreply.github.com>
2023-03-28 16:42:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ streamlit.text('🥑🍞 Avacoado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#importing data from a CSV file which is in S3 bucket and setting fruit column as index
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,4 +27,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#imported requests library and gettiing data from fruityvice website into our app
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
