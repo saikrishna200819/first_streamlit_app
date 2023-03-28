404e457c67a712a141bceae8ce9adcfc35bfc513
saikrishna200819 <128981212+saikrishna200819@users.noreply.github.com>
2023-03-28 17:28:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,4 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+streamlit.dataframe(fruityvice_response)
