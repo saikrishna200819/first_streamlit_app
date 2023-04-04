14a6818971c30d819704657b6d8b32b4ac171a8d
saikrishna200819 <128981212+saikrishna200819@users.noreply.github.com>
2023-04-04 17:08:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,7 +110,7 @@ except URLError as e:
 #streamlit.dataframe(my_data_row)
 
 #####################################################
-streamlit.header("The fruit load list contains:")
+streamlit.header("View oure fruit list - Add your Favorites!")
 #Snowflske-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -118,7 +118,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
   
 #add a button to load the fruit
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
